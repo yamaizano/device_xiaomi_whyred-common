e1666f16cabc2aebe47e9fd1bad10fc8a19196ac
yamaizano <muhammad.ikhsan.tkom421@polban.ac.id>
2024-08-23 13:44:24 +0700

    whyred-common: superior -> LineageOS

--- sdm660.mk
+++ sdm660.mk
@@ -7,7 +7,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-superior
+    $(LOCAL_PATH)/overlay-lineage
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
